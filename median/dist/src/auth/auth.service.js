3246ee8db249bfb486c1fac2b5389fdfe928a465
Dimitar Mitev <dimitar.mitev@yara.com>
2023-12-14 17:41:06 +0200

    feat(main): Add authentication (JWT, bcrypt)

--- median/dist/src/auth/auth.service.js
+++ median/dist/src/auth/auth.service.js
@@ -9,40 +9,33 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.UsersService = exports.roundsOfHashing = void 0;
+exports.AuthService = void 0;
 const common_1 = require("@nestjs/common");
 const prisma_service_1 = require("../prisma/prisma.service");
+const jwt_1 = require("@nestjs/jwt");
 const bcrypt = require("bcrypt");
-exports.roundsOfHashing = 10;
-let UsersService = class UsersService {
-    constructor(prisma) {
+let AuthService = class AuthService {
+    constructor(prisma, jwtService) {
         this.prisma = prisma;
+        this.jwtService = jwtService;
     }
-    async create(createUserDto) {
-        const hashedPassword = await bcrypt.hash(createUserDto.password, exports.roundsOfHashing);
-        createUserDto.password = hashedPassword;
-        return this.prisma.user.create({
-            data: createUserDto,
-        });
-    }
-    findAll() {
-        return this.prisma.user.findMany();
-    }
-    findOne(id) {
-        return this.prisma.user.findUnique({ where: { id } });
-    }
-    async update(id, updateUserDto) {
-        if (updateUserDto.password) {
-            updateUserDto.password = await bcrypt.hash(updateUserDto.password, exports.roundsOfHashing);
+    async login(email, password) {
+        const user = await this.prisma.user.findUnique({ where: { email: email } });
+        if (!user) {
+            throw new common_1.NotFoundException(`No user found for email: ${email}`);
         }
-    }
-    remove(id) {
-        return this.prisma.user.delete({ where: { id } });
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            throw new common_1.UnauthorizedException('Invalid password');
+        }
+        return {
+            accessToken: this.jwtService.sign({ userId: user.id }),
+        };
     }
 };
-UsersService = __decorate([
+AuthService = __decorate([
     (0, common_1.Injectable)(),
-    __metadata("design:paramtypes", [prisma_service_1.PrismaService])
-], UsersService);
-exports.UsersService = UsersService;
-//# sourceMappingURL=users.service.js.map
+    __metadata("design:paramtypes", [prisma_service_1.PrismaService, jwt_1.JwtService])
+], AuthService);
+exports.AuthService = AuthService;
+//# sourceMappingURL=auth.service.js.map
--- median/dist/src/auth/jwt-auth.guard.js
+++ median/dist/src/auth/jwt-auth.guard.js
@@ -5,22 +5,14 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var UsersModule_1;
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.UsersModule = void 0;
+exports.JwtAuthGuard = void 0;
 const common_1 = require("@nestjs/common");
-const users_service_1 = require("./users.service");
-const users_controller_1 = require("./users.controller");
-const prisma_module_1 = require("../prisma/prisma.module");
-let UsersModule = UsersModule_1 = class UsersModule {
+const passport_1 = require("@nestjs/passport");
+let JwtAuthGuard = class JwtAuthGuard extends (0, passport_1.AuthGuard)('jwt') {
 };
-UsersModule = UsersModule_1 = __decorate([
-    (0, common_1.Module)({
-        controllers: [users_controller_1.UsersController],
-        providers: [users_service_1.UsersService],
-        imports: [prisma_module_1.PrismaModule],
-        exports: [UsersModule_1],
-    })
-], UsersModule);
-exports.UsersModule = UsersModule;
-//# sourceMappingURL=users.module.js.map
+JwtAuthGuard = __decorate([
+    (0, common_1.Injectable)()
+], JwtAuthGuard);
+exports.JwtAuthGuard = JwtAuthGuard;
+//# sourceMappingURL=jwt-auth.guard.js.map
